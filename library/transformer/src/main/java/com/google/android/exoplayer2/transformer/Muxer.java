9a49213cdb2be868aba96fc6098bb06fa5175102
zhaojian2 <zhaojian2@joyy.com>
2021-12-20 10:05:55 +0800

    注释

--- library/transformer/src/main/java/com/google/android/exoplayer2/transformer/Muxer.java
+++ library/transformer/src/main/java/com/google/android/exoplayer2/transformer/Muxer.java
@@ -16,79 +16,92 @@
 package com.google.android.exoplayer2.transformer;
 
 import android.os.ParcelFileDescriptor;
+
 import androidx.annotation.Nullable;
+
 import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.util.MimeTypes;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
 /**
  * Abstracts media muxing operations.
+ * 抽象媒体多路复用操作。
  *
  * <p>Query whether {@link #supportsSampleMimeType(String) sample MIME types are supported} and
  * {@link #addTrack(Format) add all tracks}, then {@link #writeSampleData(int, ByteBuffer, boolean,
  * long) write sample data} to mux samples. Once any sample data has been written, it is not
  * possible to add tracks. After writing all sample data, {@link #release() release} the instance to
  * finish writing to the output and return any resources to the system.
+ *
+ * <p>查询{@link #supportsSampleMimeType(String) sample MIME types are supported}和
+ * {@link #addTrack(Format) add all track}，然后{@link #writeSampleData(int, ByteBuffer, boolean,
+ * long) 将样本数据}写入多路复用样本。 一旦写入任何样本数据，就无法添加曲目。
+ * 写入所有示例数据后，{@link #release() 释放} 实例以完成写入输出并将任何资源返回给系统。
  */
 /* package */ interface Muxer {
 
-  /** Factory for muxers. */
-  interface Factory {
     /**
-     * Returns a new muxer writing to a file.
-     *
-     * @param path The path to the output file.
-     * @param outputMimeType The container {@link MimeTypes MIME type} of the output file.
-     * @throws IllegalArgumentException If the path is invalid or the MIME type is not supported.
-     * @throws IOException If an error occurs opening the output file for writing.
+     * Factory for muxers.
      */
-    Muxer create(String path, String outputMimeType) throws IOException;
+    interface Factory {
+        /**
+         * Returns a new muxer writing to a file.
+         *
+         * @param path           The path to the output file.
+         * @param outputMimeType The container {@link MimeTypes MIME type} of the output file.
+         * @throws IllegalArgumentException If the path is invalid or the MIME type is not supported.
+         * @throws IOException              If an error occurs opening the output file for writing.
+         */
+        Muxer create(String path, String outputMimeType) throws IOException;
+
+        /**
+         * Returns a new muxer writing to a file descriptor.
+         *
+         * @param parcelFileDescriptor A readable and writable {@link ParcelFileDescriptor} of the
+         *                             output. The file referenced by this ParcelFileDescriptor should not be used before the
+         *                             muxer is released. It is the responsibility of the caller to close the
+         *                             ParcelFileDescriptor. This can be done after this method returns.
+         * @param outputMimeType       The {@link MimeTypes MIME type} of the output.
+         * @throws IllegalArgumentException If the file descriptor is invalid or the MIME type is not
+         *                                  supported.
+         * @throws IOException              If an error occurs opening the output file descriptor for writing.
+         */
+        Muxer create(ParcelFileDescriptor parcelFileDescriptor, String outputMimeType) throws IOException;
+
+        /**
+         * Returns whether the {@link MimeTypes MIME type} provided is a supported output format.
+         */
+        boolean supportsOutputMimeType(String mimeType);
+    }
 
     /**
-     * Returns a new muxer writing to a file descriptor.
-     *
-     * @param parcelFileDescriptor A readable and writable {@link ParcelFileDescriptor} of the
-     *     output. The file referenced by this ParcelFileDescriptor should not be used before the
-     *     muxer is released. It is the responsibility of the caller to close the
-     *     ParcelFileDescriptor. This can be done after this method returns.
-     * @param outputMimeType The {@link MimeTypes MIME type} of the output.
-     * @throws IllegalArgumentException If the file descriptor is invalid or the MIME type is not
-     *     supported.
-     * @throws IOException If an error occurs opening the output file descriptor for writing.
+     * Returns whether the sample {@link MimeTypes MIME type} is supported.
      */
-    Muxer create(ParcelFileDescriptor parcelFileDescriptor, String outputMimeType)
-        throws IOException;
+    boolean supportsSampleMimeType(@Nullable String mimeType);
 
-    /** Returns whether the {@link MimeTypes MIME type} provided is a supported output format. */
-    boolean supportsOutputMimeType(String mimeType);
-  }
+    /**
+     * Adds a track with the specified format, and returns its index (to be passed in subsequent calls
+     * to {@link #writeSampleData(int, ByteBuffer, boolean, long)}).
+     */
+    int addTrack(Format format);
 
-  /** Returns whether the sample {@link MimeTypes MIME type} is supported. */
-  boolean supportsSampleMimeType(@Nullable String mimeType);
+    /**
+     * Writes the specified sample.
+     *
+     * @param trackIndex         The index of the track, previously returned by {@link #addTrack(Format)}.
+     * @param data               Buffer containing the sample data to write to the container.
+     * @param isKeyFrame         Whether the sample is a key frame.
+     * @param presentationTimeUs The presentation time of the sample in microseconds.
+     */
+    void writeSampleData(int trackIndex, ByteBuffer data, boolean isKeyFrame, long presentationTimeUs);
 
-  /**
-   * Adds a track with the specified format, and returns its index (to be passed in subsequent calls
-   * to {@link #writeSampleData(int, ByteBuffer, boolean, long)}).
-   */
-  int addTrack(Format format);
-
-  /**
-   * Writes the specified sample.
-   *
-   * @param trackIndex The index of the track, previously returned by {@link #addTrack(Format)}.
-   * @param data Buffer containing the sample data to write to the container.
-   * @param isKeyFrame Whether the sample is a key frame.
-   * @param presentationTimeUs The presentation time of the sample in microseconds.
-   */
-  void writeSampleData(
-      int trackIndex, ByteBuffer data, boolean isKeyFrame, long presentationTimeUs);
-
-  /**
-   * Releases any resources associated with muxing.
-   *
-   * @param forCancellation Whether the reason for releasing the resources is the transformation
-   *     cancellation.
-   */
-  void release(boolean forCancellation);
+    /**
+     * Releases any resources associated with muxing.
+     *
+     * @param forCancellation Whether the reason for releasing the resources is the transformation
+     *                        cancellation.
+     */
+    void release(boolean forCancellation);
 }
