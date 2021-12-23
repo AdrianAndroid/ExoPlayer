77b209132d905effe8fb3aa56b60a7212bdde794
zhaojian2 <zhaojian2@joyy.com>
2021-12-23 10:07:21 +0800

    格式化代码

--- library/extractor/src/main/java/com/google/android/exoplayer2/extractor/ExtractorOutput.java
+++ library/extractor/src/main/java/com/google/android/exoplayer2/extractor/ExtractorOutput.java
@@ -15,55 +15,56 @@
  */
 package com.google.android.exoplayer2.extractor;
 
-/** Receives stream level data extracted by an {@link Extractor}. */
+/**
+ * Receives stream level data extracted by an {@link Extractor}.
+ */
 public interface ExtractorOutput {
 
-  /**
-   * Placeholder {@link ExtractorOutput} implementation throwing an {@link
-   * UnsupportedOperationException} in each method.
-   */
-  ExtractorOutput PLACEHOLDER =
-      new ExtractorOutput() {
+    /**
+     * Placeholder {@link ExtractorOutput} implementation throwing an {@link
+     * UnsupportedOperationException} in each method.
+     */
+    ExtractorOutput PLACEHOLDER = new ExtractorOutput() {
 
         @Override
         public TrackOutput track(int id, int type) {
-          throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException();
         }
 
         @Override
         public void endTracks() {
-          throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException();
         }
 
         @Override
         public void seekMap(SeekMap seekMap) {
-          throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException();
         }
-      };
+    };
 
-  /**
-   * Called by the {@link Extractor} to get the {@link TrackOutput} for a specific track.
-   *
-   * <p>The same {@link TrackOutput} is returned if multiple calls are made with the same {@code
-   * id}.
-   *
-   * @param id A track identifier.
-   * @param type The type of the track. Typically one of the {@link com.google.android.exoplayer2.C}
-   *     {@code TRACK_TYPE_*} constants.
-   * @return The {@link TrackOutput} for the given track identifier.
-   */
-  TrackOutput track(int id, int type);
+    /**
+     * Called by the {@link Extractor} to get the {@link TrackOutput} for a specific track.
+     *
+     * <p>The same {@link TrackOutput} is returned if multiple calls are made with the same {@code
+     * id}.
+     *
+     * @param id   A track identifier.
+     * @param type The type of the track. Typically one of the {@link com.google.android.exoplayer2.C}
+     *             {@code TRACK_TYPE_*} constants.
+     * @return The {@link TrackOutput} for the given track identifier.
+     */
+    TrackOutput track(int id, int type);
 
-  /**
-   * Called when all tracks have been identified, meaning no new {@code trackId} values will be
-   * passed to {@link #track(int, int)}.
-   */
-  void endTracks();
+    /**
+     * Called when all tracks have been identified, meaning no new {@code trackId} values will be
+     * passed to {@link #track(int, int)}.
+     */
+    void endTracks();
 
-  /**
-   * Called when a {@link SeekMap} has been extracted from the stream.
-   *
-   * @param seekMap The extracted {@link SeekMap}.
-   */
-  void seekMap(SeekMap seekMap);
+    /**
+     * Called when a {@link SeekMap} has been extracted from the stream.
+     *
+     * @param seekMap The extracted {@link SeekMap}.
+     */
+    void seekMap(SeekMap seekMap);
 }
