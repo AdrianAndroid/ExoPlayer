b9aa5c784799503bc527471a6d46a3e52928c886
zhaojian2 <zhaojian2@joyy.com>
2021-12-27 14:16:08 +0800

    格式化代码

--- library/common/src/main/java/com/google/android/exoplayer2/upstream/DataReader.java
+++ library/common/src/main/java/com/google/android/exoplayer2/upstream/DataReader.java
@@ -18,23 +18,28 @@ package com.google.android.exoplayer2.upstream;
 import com.google.android.exoplayer2.C;
 import java.io.IOException;
 
-/** Reads bytes from a data stream. */
+/**
+ * Reads bytes from a data stream. 从数据流中读取字节。
+ */
 public interface DataReader {
-  /**
-   * Reads up to {@code length} bytes of data from the input.
-   *
-   * <p>If {@code readLength} is zero then 0 is returned. Otherwise, if no data is available because
-   * the end of the opened range has been reached, then {@link C#RESULT_END_OF_INPUT} is returned.
-   * Otherwise, the call will block until at least one byte of data has been read and the number of
-   * bytes read is returned.
-   *
-   * @param buffer A target array into which data should be written.
-   * @param offset The offset into the target array at which to write.
-   * @param length The maximum number of bytes to read from the input.
-   * @return The number of bytes read, or {@link C#RESULT_END_OF_INPUT} if the input has ended. This
-   *     may be less than {@code length} because the end of the input (or available data) was
-   *     reached, the method was interrupted, or the operation was aborted early for another reason.
-   * @throws IOException If an error occurs reading from the input.
-   */
-  int read(byte[] buffer, int offset, int length) throws IOException;
+    /**
+     * Reads up to {@code length} bytes of data from the input. 从输入中读取最多长度字节的数据。
+     *
+     * <p>If {@code readLength} is zero then 0 is returned. Otherwise, if no data is available because
+     * the end of the opened range has been reached, then {@link C#RESULT_END_OF_INPUT} is returned.
+     * Otherwise, the call will block until at least one byte of data has been read and the number of
+     * bytes read is returned. 如果readLength为零，则返回0。否则，如果由于已达到打开范围的末尾而没有可用数据，则
+     * 返回RESULT_END_OF_INPUT。否则，调用将阻塞，直到至少读取了一个字节的数据并返回读取的字节数。
+     *
+     * @param buffer A target array into which data should be written. 应写入数据的目标数组。
+     * @param offset The offset into the target array at which to write. 写入目标数组的偏移量。
+     * @param length The maximum number of bytes to read from the input.  从输入读取的最大字节数。
+     * @return The number of bytes read, or {@link C#RESULT_END_OF_INPUT} if the input has ended. This
+     * may be less than {@code length} because the end of the input (or available data) was
+     * reached, the method was interrupted, or the operation was aborted early for another reason.
+     * 读取的字节数，如果输入已经结束，则为RESULT_END_OF_INPUT。这可能小于Length，因为已经达到输入（或可用数据）的末尾，
+     * 方法被终端或操作原因提前中止。
+     * @throws IOException If an error occurs reading from the input.
+     */
+    int read(byte[] buffer, int offset, int length) throws IOException;
 }
--- library/extractor/src/main/java/com/google/android/exoplayer2/extractor/TrackOutput.java
+++ library/extractor/src/main/java/com/google/android/exoplayer2/extractor/TrackOutput.java
@@ -28,174 +28,189 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.Arrays;
 
-/** Receives track level data extracted by an {@link Extractor}. */
+/**
+ * Receives track level data extracted by an {@link Extractor}.
+ */
 public interface TrackOutput {
 
-  /** Holds data required to decrypt a sample. */
-  final class CryptoData {
-
-    /** The encryption mode used for the sample. */
-    @C.CryptoMode public final int cryptoMode;
-
-    /** The encryption key associated with the sample. Its contents must not be modified. */
-    public final byte[] encryptionKey;
-
     /**
-     * The number of encrypted blocks in the encryption pattern, 0 if pattern encryption does not
-     * apply.
+     * Holds data required to decrypt a sample.
      */
-    public final int encryptedBlocks;
+    final class CryptoData {
 
-    /**
-     * The number of clear blocks in the encryption pattern, 0 if pattern encryption does not apply.
-     */
-    public final int clearBlocks;
+        /**
+         * The encryption mode used for the sample.
+         */
+        @C.CryptoMode
+        public final int cryptoMode;
 
-    /**
-     * @param cryptoMode See {@link #cryptoMode}.
-     * @param encryptionKey See {@link #encryptionKey}.
-     * @param encryptedBlocks See {@link #encryptedBlocks}.
-     * @param clearBlocks See {@link #clearBlocks}.
-     */
-    public CryptoData(@C.CryptoMode int cryptoMode, byte[] encryptionKey, int encryptedBlocks, int clearBlocks) {
-      this.cryptoMode = cryptoMode;
-      this.encryptionKey = encryptionKey;
-      this.encryptedBlocks = encryptedBlocks;
-      this.clearBlocks = clearBlocks;
+        /**
+         * The encryption key associated with the sample. Its contents must not be modified.
+         */
+        public final byte[] encryptionKey;
+
+        /**
+         * The number of encrypted blocks in the encryption pattern, 0 if pattern encryption does not
+         * apply.
+         */
+        public final int encryptedBlocks;
+
+        /**
+         * The number of clear blocks in the encryption pattern, 0 if pattern encryption does not apply.
+         */
+        public final int clearBlocks;
+
+        /**
+         * @param cryptoMode      See {@link #cryptoMode}.
+         * @param encryptionKey   See {@link #encryptionKey}.
+         * @param encryptedBlocks See {@link #encryptedBlocks}.
+         * @param clearBlocks     See {@link #clearBlocks}.
+         */
+        public CryptoData(@C.CryptoMode int cryptoMode, byte[] encryptionKey, int encryptedBlocks, int clearBlocks) {
+            this.cryptoMode = cryptoMode;
+            this.encryptionKey = encryptionKey;
+            this.encryptedBlocks = encryptedBlocks;
+            this.clearBlocks = clearBlocks;
+        }
+
+        @Override
+        public boolean equals(@Nullable Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            CryptoData other = (CryptoData) obj;
+            return cryptoMode == other.cryptoMode
+                    && encryptedBlocks == other.encryptedBlocks
+                    && clearBlocks == other.clearBlocks
+                    && Arrays.equals(encryptionKey, other.encryptionKey);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = cryptoMode;
+            result = 31 * result + Arrays.hashCode(encryptionKey);
+            result = 31 * result + encryptedBlocks;
+            result = 31 * result + clearBlocks;
+            return result;
+        }
     }
 
-    @Override
-    public boolean equals(@Nullable Object obj) {
-      if (this == obj) {
-        return true;
-      }
-      if (obj == null || getClass() != obj.getClass()) {
-        return false;
-      }
-      CryptoData other = (CryptoData) obj;
-      return cryptoMode == other.cryptoMode
-          && encryptedBlocks == other.encryptedBlocks
-          && clearBlocks == other.clearBlocks
-          && Arrays.equals(encryptionKey, other.encryptionKey);
+    /**
+     * Defines the part of the sample data to which a call to {@link #sampleData} corresponds.
+     */
+    @Documented
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({SAMPLE_DATA_PART_MAIN, SAMPLE_DATA_PART_ENCRYPTION, SAMPLE_DATA_PART_SUPPLEMENTAL})
+    @interface SampleDataPart {
     }
 
-    @Override
-    public int hashCode() {
-      int result = cryptoMode;
-      result = 31 * result + Arrays.hashCode(encryptionKey);
-      result = 31 * result + encryptedBlocks;
-      result = 31 * result + clearBlocks;
-      return result;
+    /**
+     * Main media sample data.
+     */
+    int SAMPLE_DATA_PART_MAIN = 0;
+    /**
+     * Sample encryption data. 样本加密数据
+     *
+     * <p>The format for encryption information is: 加密信息的格式为：
+     *
+     * <ul>
+     *   <li>(1 byte) {@code encryption_signal_byte}: Most significant bit signals whether the
+     *       encryption data contains subsample encryption data. The remaining bits contain {@code
+     *       initialization_vector_size}.
+     *   <li>({@code initialization_vector_size} bytes) Initialization vector.
+     *   <li>If subsample encryption data is present, as per {@code encryption_signal_byte}, the
+     *       encryption data also contains:
+     *       <ul>
+     *         <li>(2 bytes) {@code subsample_encryption_data_length}.
+     *         <li>({@code subsample_encryption_data_length * 6} bytes) Subsample encryption data
+     *             (repeated {@code subsample_encryption_data_length} times:
+     *             <ul>
+     *               <li>(2 bytes) Size of a clear section in sample.
+     *               <li>(4 bytes) Size of an encryption section in sample.
+     *             </ul>
+     *       </ul>
+     * </ul>
+     */
+    int SAMPLE_DATA_PART_ENCRYPTION = 1;
+    /**
+     * Sample supplemental data.
+     *
+     * <p>If a sample contains supplemental data, the format of the entire sample data will be:
+     *
+     * <ul>
+     *   <li>If the sample has the {@link C#BUFFER_FLAG_ENCRYPTED} flag set, all encryption
+     *       information.
+     *   <li>(4 bytes) {@code sample_data_size}: The size of the actual sample data, not including
+     *       supplemental data or encryption information.
+     *   <li>({@code sample_data_size} bytes): The media sample data.
+     *   <li>(remaining bytes) The supplemental data.
+     * </ul>
+     */
+    int SAMPLE_DATA_PART_SUPPLEMENTAL = 2;
+
+    /**
+     * Called when the {@link Format} of the track has been extracted from the stream. 当从流提取轨道的格式时调用。
+     *
+     * @param format The extracted {@link Format}.
+     */
+    void format(Format format);
+
+    /**
+     * Equivalent to {@link #sampleData(DataReader, int, boolean, int) sampleData(input, length,
+     * allowEndOfInput, SAMPLE_DATA_PART_MAIN)}.
+     */
+    default int sampleData(DataReader input, int length, boolean allowEndOfInput) throws IOException {
+        return sampleData(input, length, allowEndOfInput, SAMPLE_DATA_PART_MAIN);
     }
-  }
 
-  /** Defines the part of the sample data to which a call to {@link #sampleData} corresponds. */
-  @Documented
-  @Retention(RetentionPolicy.SOURCE)
-  @IntDef({SAMPLE_DATA_PART_MAIN, SAMPLE_DATA_PART_ENCRYPTION, SAMPLE_DATA_PART_SUPPLEMENTAL})
-  @interface SampleDataPart {}
+    /**
+     * Equivalent to {@link #sampleData(ParsableByteArray, int, int)} sampleData(data, length,
+     * SAMPLE_DATA_PART_MAIN)}.
+     */
+    default void sampleData(ParsableByteArray data, int length) {
+        sampleData(data, length, SAMPLE_DATA_PART_MAIN);
+    }
 
-  /** Main media sample data. */
-  int SAMPLE_DATA_PART_MAIN = 0;
-  /**
-   * Sample encryption data.
-   *
-   * <p>The format for encryption information is:
-   *
-   * <ul>
-   *   <li>(1 byte) {@code encryption_signal_byte}: Most significant bit signals whether the
-   *       encryption data contains subsample encryption data. The remaining bits contain {@code
-   *       initialization_vector_size}.
-   *   <li>({@code initialization_vector_size} bytes) Initialization vector.
-   *   <li>If subsample encryption data is present, as per {@code encryption_signal_byte}, the
-   *       encryption data also contains:
-   *       <ul>
-   *         <li>(2 bytes) {@code subsample_encryption_data_length}.
-   *         <li>({@code subsample_encryption_data_length * 6} bytes) Subsample encryption data
-   *             (repeated {@code subsample_encryption_data_length} times:
-   *             <ul>
-   *               <li>(2 bytes) Size of a clear section in sample.
-   *               <li>(4 bytes) Size of an encryption section in sample.
-   *             </ul>
-   *       </ul>
-   * </ul>
-   */
-  int SAMPLE_DATA_PART_ENCRYPTION = 1;
-  /**
-   * Sample supplemental data.
-   *
-   * <p>If a sample contains supplemental data, the format of the entire sample data will be:
-   *
-   * <ul>
-   *   <li>If the sample has the {@link C#BUFFER_FLAG_ENCRYPTED} flag set, all encryption
-   *       information.
-   *   <li>(4 bytes) {@code sample_data_size}: The size of the actual sample data, not including
-   *       supplemental data or encryption information.
-   *   <li>({@code sample_data_size} bytes): The media sample data.
-   *   <li>(remaining bytes) The supplemental data.
-   * </ul>
-   */
-  int SAMPLE_DATA_PART_SUPPLEMENTAL = 2;
+    /**
+     * Called to write sample data to the output. 调用以将示例数据写入输出。
+     *
+     * @param input           A {@link DataReader} from which to read the sample data. 从中读取示例数据的DataReader.
+     * @param length          The maximum length to read from the input.  从输入读取的最大长度。
+     * @param allowEndOfInput True if encountering the end of the input having read no data is
+     *                        allowed, and should result in {@link C#RESULT_END_OF_INPUT} being returned. False if it
+     *                        should be considered an error, causing an {@link EOFException} to be thrown.
+     *                        如果遇到没有读取任何数据的输入的结尾，则为True，并且应导致返回RESULT_END_OF_INPUT。如果应该在错误时考虑它，则为False，导致抛出异常
+     * @param sampleDataPart  The part of the sample data to which this call corresponds. 此调用对应的样本数据部分。
+     * @return The number of bytes appended.  附加的字节数.
+     * @throws IOException If an error occurred reading from the input.  如果从输入读取时发生错误。
+     */
+    int sampleData(DataReader input, int length, boolean allowEndOfInput, @SampleDataPart int sampleDataPart) throws IOException;
 
-  /**
-   * Called when the {@link Format} of the track has been extracted from the stream.
-   *
-   * @param format The extracted {@link Format}.
-   */
-  void format(Format format);
+    /**
+     * Called to write sample data to the output.å
+     *
+     * @param data           A {@link ParsableByteArray} from which to read the sample data.
+     * @param length         The number of bytes to read, starting from {@code data.getPosition()}.
+     * @param sampleDataPart The part of the sample data to which this call corresponds.
+     */
+    void sampleData(ParsableByteArray data, int length, @SampleDataPart int sampleDataPart);
 
-  /**
-   * Equivalent to {@link #sampleData(DataReader, int, boolean, int) sampleData(input, length,
-   * allowEndOfInput, SAMPLE_DATA_PART_MAIN)}.
-   */
-  default int sampleData(DataReader input, int length, boolean allowEndOfInput) throws IOException {
-    return sampleData(input, length, allowEndOfInput, SAMPLE_DATA_PART_MAIN);
-  }
-
-  /**
-   * Equivalent to {@link #sampleData(ParsableByteArray, int, int)} sampleData(data, length,
-   * SAMPLE_DATA_PART_MAIN)}.
-   */
-  default void sampleData(ParsableByteArray data, int length) {
-    sampleData(data, length, SAMPLE_DATA_PART_MAIN);
-  }
-
-  /**
-   * Called to write sample data to the output.
-   *
-   * @param input A {@link DataReader} from which to read the sample data.
-   * @param length The maximum length to read from the input.
-   * @param allowEndOfInput True if encountering the end of the input having read no data is
-   *     allowed, and should result in {@link C#RESULT_END_OF_INPUT} being returned. False if it
-   *     should be considered an error, causing an {@link EOFException} to be thrown.
-   * @param sampleDataPart The part of the sample data to which this call corresponds.
-   * @return The number of bytes appended.
-   * @throws IOException If an error occurred reading from the input.
-   */
-  int sampleData(DataReader input, int length, boolean allowEndOfInput, @SampleDataPart int sampleDataPart) throws IOException;
-
-  /**
-   * Called to write sample data to the output.
-   *
-   * @param data A {@link ParsableByteArray} from which to read the sample data.
-   * @param length The number of bytes to read, starting from {@code data.getPosition()}.
-   * @param sampleDataPart The part of the sample data to which this call corresponds.
-   */
-  void sampleData(ParsableByteArray data, int length, @SampleDataPart int sampleDataPart);
-
-  /**
-   * Called when metadata associated with a sample has been extracted from the stream.
-   *
-   * <p>The corresponding sample data will have already been passed to the output via calls to
-   * {@link #sampleData(DataReader, int, boolean)} or {@link #sampleData(ParsableByteArray, int)}.
-   *
-   * @param timeUs The media timestamp associated with the sample, in microseconds.
-   * @param flags Flags associated with the sample. See {@code C.BUFFER_FLAG_*}.
-   * @param size The size of the sample data, in bytes.
-   * @param offset The number of bytes that have been passed to {@link #sampleData(DataReader, int,
-   *     boolean)} or {@link #sampleData(ParsableByteArray, int)} since the last byte belonging to
-   *     the sample whose metadata is being passed.
-   * @param cryptoData The encryption data required to decrypt the sample. May be null.
-   */
-  void sampleMetadata(long timeUs, @C.BufferFlags int flags, int size, int offset, @Nullable CryptoData cryptoData);
+    /**
+     * Called when metadata associated with a sample has been extracted from the stream.
+     *
+     * <p>The corresponding sample data will have already been passed to the output via calls to
+     * {@link #sampleData(DataReader, int, boolean)} or {@link #sampleData(ParsableByteArray, int)}.
+     *
+     * @param timeUs     The media timestamp associated with the sample, in microseconds.
+     * @param flags      Flags associated with the sample. See {@code C.BUFFER_FLAG_*}.
+     * @param size       The size of the sample data, in bytes.
+     * @param offset     The number of bytes that have been passed to {@link #sampleData(DataReader, int,
+     *                   boolean)} or {@link #sampleData(ParsableByteArray, int)} since the last byte belonging to
+     *                   the sample whose metadata is being passed.
+     * @param cryptoData The encryption data required to decrypt the sample. May be null.
+     */
+    void sampleMetadata(long timeUs, @C.BufferFlags int flags, int size, int offset, @Nullable CryptoData cryptoData);
 }
